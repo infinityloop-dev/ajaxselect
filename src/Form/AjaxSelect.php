eaeef3a0beef541292c0734304514315e7ca955d
Václav Pelíšek <peldax@gmail.com>
2019-01-13 18:02:31 +0100

    Create AjaxSelect.php

---
+++ src/Form/AjaxSelect.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+declare(strict_types=1);
+
+namespace Nepttune\Form;
+
+class AjaxSelect extends \Nette\Forms\Controls\SelectBox implements \Nette\Application\UI\ISignalReceiver
+{
+    use TAjaxSelect;
+
+    public const SIGNAL_NAME = 'load';
+
+    public function __construct(?string $label, callable $callback)
+    {
+        $this->callback = $callback;
+        parent::__construct($label);
+    }
+}
