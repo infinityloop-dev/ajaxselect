d04e6d271957f2b9008f222e20008beab039ca22
Václav Pelíšek <peldax@gmail.com>
2019-01-13 18:03:14 +0100

    Create AjaxMultiSelect.php

---
+++ src/Form/AjaxMultiSelect.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2019 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+declare(strict_types=1);
+
+namespace Nepttune\Form;
+
+class AjaxMultiSelect extends \Nette\Forms\Controls\MultiSelectBox implements \Nette\Application\UI\ISignalReceiver
+{
+    use TAjaxSelect;
+
+    public const SIGNAL_NAME = AjaxSelect::SIGNAL_NAME;
+
+    public function __construct(?string $label, callable $callback)
+    {
+        $this->callback = $callback;
+        parent::__construct($label);
+    }
+}
